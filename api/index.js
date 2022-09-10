8cb02ba03980986e14ba23fd73eac688a78c1549
Liu Wenyuan <15816141883@163.com>
2022-09-10 20:22:43 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ import { renderFile } from "ejs";
 module.exports = function (req, res) {
     renderFile("../template.html")
     .catch(function (err) {
+        err.errno = 500;
+        err.message = "Error ocurred while rendering embed";
         console.error(err);
-        res.status(500).send(err);
+        res.status(500).json(err);
     })
     .then(data => res.send(data));
 };
