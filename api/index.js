d1d597702d4bd0fb0328dcca4eee3cc26c2f3507
Liu Wenyuan <15816141883@163.com>
2022-09-10 20:31:49 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,29 @@
-import { renderFile } from "ejs";
+import { render, renderFile } from "ejs";
+
+const ERROR_TEMPLATE = `
+<!DOCTYPE HTML>
+<head>
+    <title>bembedfix - Error: <%= message %></title>
+    <meta property="og:title" content="<%= code %> - <%= message %>" />
+    <meta name="theme-color" content="#FF0000">
+</head>
+<body>
+    <h1><%= code %> - <%= message %></h1>
+    <p><pre><%= data %></pre></p>
+</body>
+`;
+
+function generateError(code, message, data) {
+    return render(ERROR_TEMPLATE, { code: code, message: message, data: data });
+}
 
 module.exports = function (req, res) {
     renderFile("../template.html")
     .catch(function (err) {
-        err.errno = 500;
-        err.message = "Error ocurred while rendering embed";
         console.error(err);
-        res.status(500).json(err);
+        res
+            .status(500)
+            .send(generateError(500, "An error ocurred while rendering the embed", err));
     })
     .then(data => res.send(data));
 };
