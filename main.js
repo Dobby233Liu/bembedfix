e94be10f7b211d1563e9c06af1c92797fe98d23f
Liu Wenyuan <15816141883@163.com>
2022-09-17 12:50:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 import { sendOembed, sendTemplate, generateError } from "./utils.js";
-import { getVideoIdByPath, getVideoData, getOembedData } from "./utils_bilibili.js";
+import { getVideoIdByPath, grabVideoStreamFromBVID, getVideoData, getOembedData } from "./utils_bilibili.js";
 import { PROJECT_URL, PROVIDER_NAME, CRAWLER_UAS } from "./conf.js";
 
 export default function handler(req, res) {
+    res.setHeader("Cache-Control", "max-age=0, s-maxage=0");
+
     let parsableURL = new URL(req.url, "https://" + req.headers.host);
 
     if (parsableURL.pathname == "/favicon.ico") {
         res.setHeader("Cache-Control", "max-age=86400, s-maxage=86400");
         res.redirect(301, "https://www.bilibili.com/favicon.ico");
+        return;
+    }
+
+    if (parsableURL.pathname == "/getvideo") {
+        grabVideoStreamFromBVID(req.query.bvid).pipe(res);
         return;
     }
 
