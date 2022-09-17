022b7d95bdbd3a3622e49655ec3ce3f7a55424ed
Liu Wenyuan <15816141883@163.com>
2022-09-17 16:14:06 +0800

    Update videoplayback.py

--- api/videoplayback.py
+++ api/videoplayback.py
@@ -23,15 +23,14 @@ class handler(BaseHTTPRequestHandler):
 
         parsed = urlparse(f"http://fake{self.path}")
         query = dict(parse_qsl(parsed.query))
-        self.wfile.write(query.bvid.encode())
+        self.wfile.write(query["bvid"].encode())
 
         try:
             with redirect_stdout(self.wfile), yt_dlp.YoutubeDL(conf) as ydl:
-                ydl.download("https://www.bilibili.com/video/" + query.bvid)
+                ydl.download("https://www.bilibili.com/video/" + query["bvid"])
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             error_text = traceback.format_tb(exc_traceback)
-            self.wfile.write(error_text.encode())
+            print(error_text, file=self.wfile)
 
-        self.wfile.flush()
         return
