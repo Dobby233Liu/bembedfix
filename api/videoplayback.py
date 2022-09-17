4245f709857bba26a6c5676cbc7b19d5398de30e
Liu Wenyuan <15816141883@163.com>
2022-09-17 16:22:59 +0800

    Update videoplayback.py

--- api/videoplayback.py
+++ api/videoplayback.py
@@ -1,12 +1,14 @@
 from http.server import BaseHTTPRequestHandler
-from contextlib import redirect_stdout
+from contextlib import redirect_stdout, redirect_stderr
 import yt_dlp
 from urllib.parse import urlparse, parse_qsl
 import traceback
 import sys
+import io
 
 conf = {
     "outtmpl": "-",
+    "logtostderr": True,
     "format": "bestvideo*+bestaudio/best",
     "postprocessors": [{
         "key": "FFmpegVideoRemuxer",
@@ -26,11 +28,11 @@ class handler(BaseHTTPRequestHandler):
         query = dict(parse_qsl(parsed.query))
 
         try:
-            with redirect_stdout(self.wfile), yt_dlp.YoutubeDL(conf) as ydl:
+            with redirect_stdout(self.wfile), redirect_stderr(io.StringIO()), yt_dlp.YoutubeDL(conf) as ydl:
                 ydl.download("https://www.bilibili.com/video/" + query["bvid"])
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             error_text = traceback.format_tb(exc_traceback)
-            print(error_text, file=self.wfile)
+            self.wfile.write(error_text.encode())
 
         return
