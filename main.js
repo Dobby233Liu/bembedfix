8ed33e4024179e9f2b9dec3d180b6a002b7c65c5
Liu Wenyuan <15816141883@163.com>
2022-11-19 13:24:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,20 +19,53 @@ export default function handler(req, res) {
     res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
 
     if (requestedURL.pathname == "/oembed" || requestedURL.pathname == "/oembed.json" || requestedURL.pathname == "/oembed.xml") {
-        sendOembed(getOembedData(req.query), res,
-            requestedURL.pathname.endsWith(".xml")
-            || (!requestedURL.pathname.endsWith(".json") && req.query.format == "xml"));
+        var isXMLRequested = requestedURL.pathname.endsWith(".xml")
+            || (!requestedURL.pathname.endsWith(".json") && req.query.format == "xml");
+
+        if (req.query.url) {
+            // user requested with a URL, grab video info
+            // FIXME: video
+
+            getVideoIdByPathSmart(new URL(req.query.url).pathname)
+            .then(id => {
+                getVideoData(id)
+                .then(data => {
+                    sendOembed(getOembedData({
+                        type: "video",
+                        bvid: data.bvid,
+                        pic: data.thumbnail,
+                        author: data.author,
+                        mid: data.author_mid,
+                        maxwidth: req.query.maxwidth,
+                        maxheight: req.query.maxheight,
+                    }), res, isXMLRequested);
+                })
+                .catch(e => {
+                    res
+                        .status(500)
+                        .send(generateError(500, "获取视频信息时发生错误", e, req));
+                });
+            })
+            .catch(e => {
+                res
+                    .status(500)
+                    .send(generateError(500, "解析请求的 URL 时发生错误", e, req));
+            });
+
+            return;
+        }
+
+        sendOembed(getOembedData(req.query), res, isXMLRequested);
         return;
     }
 
-    // FIXME: preserve some queries
     getVideoIdByPathSmart(requestedURL.pathname)
     .then(id => {
         getVideoData(id)
         .then(data => {
             if (!CRAWLER_UAS.includes(req.headers["user-agent"])
                 && !req.query.__bef_tag_debug) {
-                res.setHeader("Cache-Control", "IGNORE");
+                res.setHeader("Cache-Control", "private, max-age=1, stale-while-revalidate");
                 // redirect the client to the real video URL
                 res.redirect(302, data.url);
                 return;
