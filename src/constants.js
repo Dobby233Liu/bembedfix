0756af832add15c6ec1bca63e33c89cabc3b4f2b
Liu Wenyuan <15816141883@163.com>
2024-07-17 10:20:10 +0800

    Add 2 Twitter UAs

--- src/constants.js
+++ src/constants.js
@@ -6,9 +6,12 @@ export const PROJECT_URL = "https://github.com/Dobby233Liu/bembedfix";
 export const PROJECT_ISSUES_URL =
     "https://github.com/Dobby233Liu/bembedfix/issues/new";
 
+// TODO: Maybe make some of these RegExes in case of oddballs like The Lounge IRC Client (like Twitterbot)
 export const CRAWLER_UAS = [
     "facebookexternalhit/1.1",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.4 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.4 facebookexternalhit/1.1 Facebot Twitterbot/1.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 Twitterbot/1.0",
+    "Twitterbot/1.0",
     "Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)",
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0",
@@ -19,7 +22,7 @@ export const CRAWLER_UAS = [
     "Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US; Valve Steam Client/default/1596241936; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
     "Mozilla/5.0 (Windows; U; Windows NT 10.0; en-US; Valve Steam Client/default/0; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
     "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; Valve Steam FriendsUI Tenfoot/0; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
-    "BembedfixMetaTagDebugging (like test)",
+    "BembedfixMetaTagDebugging (like test)"
 ];
 
 export const ERROR_TEMPLATE = `<!DOCTYPE HTML>
