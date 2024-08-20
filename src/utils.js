abae74cc3d2be71cdc68ae11cc9d2db0c2e55da8
Liu Wenyuan <15816141883@163.com>
2024-08-21 06:50:07 +0800

    Enable global mode for multiple slashes regex

--- src/utils.js
+++ src/utils.js
@@ -47,7 +47,7 @@ export function checkIfURLIsUnderDomain(l, r) {
 }
 
 export function stripTrailingSlashes(path) {
-    return path.replace(/\/+/, "/").replace(/\/+$/, "");
+    return path.replace(/\/+/g, "/").replace(/\/+$/, "");
 }
 
 export function isUserAStupidKidAndTryingToAccessAWordpressApi(url) {
